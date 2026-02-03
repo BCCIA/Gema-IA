a76a4037372cb13e9693222fbb267d308022411c
BCCIA <tecnologia@bccvzla.com>
2026-02-03 13:19:08 -0400

    Update chat URL in DIDChat class

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ class DIDChat {
   constructor(containerId) {
     this.container = document.getElementById(containerId);
     this.chatUrl =
-      "https://studio.d-id.com/agents/share?id=agt_frUC70lI&utm_source=copy&key=WjI5dloyeGxMVzloZFhSb01ud3hNRE01TlRNNU9UYzFNRFExTmpVek5qWTFOak02Wm1aaFQzbENaVU5KV1ZOamQzSTBkRkp5TUdGNg==";
+      "https://studio.d-id.com/agents/share?id=v2_agt_frUC70lI&utm_source=copy&key=WjI5dloyeGxMVzloZFhSb01ud3hNRE01TlRNNU9UYzFNRFExTmpVek5qWTFOak02Wm1aaFQzbENaVU5KV1ZOamQzSTBkRkp5TUdGNg==";
     this.iframe = null;
     this.init();
   }
